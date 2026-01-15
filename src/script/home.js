e446a69069ac5699f827781a3b915234d1bd7278
Divansir-Junior <jscrobut989@gmail.com>
2026-01-15 11:37:41 -0300

    (STYLE) Home page created

---
+++ src/script/home.js
@@ -0,0 +1,31 @@
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  dots: '.dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  },
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    }
+  ]
+});
+
+
+function readHomePage() {
+  const btnGoToProducts = document.getElementById("readMore");
+  const target = document.getElementById("products");
+
+  btnGoToProducts.addEventListener("click", () => {
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+}
+
+readHomePage();
+lucide.createIcons();
