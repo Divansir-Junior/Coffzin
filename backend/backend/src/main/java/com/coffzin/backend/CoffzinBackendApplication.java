df41569587fe35c147076899257d9c5da204832a
Divansir-Junior <jscrobut989@gmail.com>
2026-01-26 20:41:02 -0300

    (CHANGED) Folders structure changed

---
+++ backend/backend/src/main/java/com/coffzin/backend/CoffzinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coffzin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffzinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffzinBackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/coffzin/backend/CoffzinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffzin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffzinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
