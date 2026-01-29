c7de3a7fc55b078ea56a672076befccd1e9a0bb7
Divansir-Junior <jscrobut989@gmail.com>
2026-01-29 00:30:47 -0300

    Feature : The homepage's shopping button now redirects to the product page.

--- frontend/src/script/util/redPages.js
+++ frontend/src/script/util/redPages.js
@@ -2,6 +2,10 @@
 function redirectToPages() {
   const btnOurProducts = document.getElementById("prodtBtn");
   const btnAboutUs = document.getElementById("contactBtn");
+  const btnBuyNow = document.getElementById("buyBtn");
+  btnBuyNow.addEventListener("click", () => {
+    window.location.href = "../pages/products.html";
+  });
   btnAboutUs.addEventListener("click", () => {
     window.location.href = "../pages/contact.html";
   });
