525bcb697aacf7b86c86757afa6aef816a90c4b9
Divansir-Junior <jscrobut989@gmail.com>
2026-02-12 12:39:05 -0300

    feat : create user method to save and update by id

--- backend/coffzin_backend/src/main/java/com/coffzin/controller/ProductController.java
+++ backend/coffzin_backend/src/main/java/com/coffzin/controller/ProductController.java
@@ -1,5 +1,50 @@
 package com.coffzin.controller;
 
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coffzin.model.Product;
+import com.coffzin.repository.ProductRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*") 
 public class ProductController {
 
+    private final ProductRepository productRepository;
+
+    @Operation(summary = "Create a new product")
+    @PostMapping
+    public ResponseEntity <Product> saveProduct(@RequestBody Product product) {
+        Product savedProduct = productRepository.save(product);
+        return ResponseEntity.ok(savedProduct);
+    }
+
+    @Operation(summary = "Get all products")
+    @GetMapping 
+    public List <Product> getAllProducts () {
+        return productRepository.findAll();
+    }
+
+    @Operation(summary = "Get product by ID")
+    @GetMapping("/{id}")
+    public ResponseEntity <Product> getProductById (@PathVariable Long id) { 
+        return productRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
--- backend/coffzin_backend/src/main/java/com/coffzin/repository/ProductRepository.java
+++ backend/coffzin_backend/src/main/java/com/coffzin/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.coffzin.repository;
 
-public class ProductRepository {
+import com.coffzin.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface  ProductRepository extends JpaRepository<Product, Long> {
 
 }
