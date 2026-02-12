0e9eb9556a1f4ad30893cfc829ea1416b7ffe69a
Divansir-Junior <jscrobut989@gmail.com>
2026-02-12 12:18:54 -0300

    feat : add all product informations to the class

--- backend/coffzin_backend/src/main/java/com/coffzin/model/Product.java
+++ backend/coffzin_backend/src/main/java/com/coffzin/model/Product.java
@@ -1,14 +1,19 @@
 package com.coffzin.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "products")
+@Data
 @Getter
 @Setter
 @AllArgsConstructor
@@ -17,8 +22,14 @@ public class Product {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String description;
+
+    @Column(nullable = false)
     private double price;
     
 }
