fc9fe8980788d894e3a9a1dfccf96944646bd0ae
Divansir-Junior <jscrobut989@gmail.com>
2026-02-14 17:10:31 -0300

    fix(entity): add @Id to Message entity and include date field

--- backend/coffzin_backend/src/main/java/com/coffzin/model/Message.java
+++ backend/coffzin_backend/src/main/java/com/coffzin/model/Message.java
@@ -1,7 +1,13 @@
 package com.coffzin.model;
 
+import java.time.LocalDate;
+
+import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -14,7 +20,14 @@ import lombok.Setter;
 @Table(name = "messages")
 public class Message {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(name = "content", nullable = false)
     private String content;
+
+    @Column(name = "date", nullable = false)    
+    private LocalDate date;
 
 }
