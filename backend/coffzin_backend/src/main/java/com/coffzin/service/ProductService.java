c9fcb6313a0244bf3405777ac08ec69bc34d78eb
Divansir-Junior <jscrobut989@gmail.com>
2026-02-15 23:01:03 -0300

    feat : update and delete function created

--- backend/coffzin_backend/src/main/java/com/coffzin/service/ProductService.java
+++ backend/coffzin_backend/src/main/java/com/coffzin/service/ProductService.java
@@ -26,5 +26,30 @@ public class ProductService {
         
     }
 
+    public ProductResponseDTO getProductById(Long id) { 
+        return productRepository.findById(id).
+                map(ProductResponseDTO::fromEntity)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+    }
+
+    public void deleteProduct (Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new RuntimeException("Product not found with id: " + id);
+        }
+
+        productRepository.deleteById(id);
+    }
+
+    public ProductResponseDTO updateProduct (Long id, ProductRequestDTO request) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setPrice(request.getPrice());
+        product.setQuantity(request.getStockQuantity());
+
+        return ProductResponseDTO.fromEntity(productRepository.save(product));
+    }
 
 }
