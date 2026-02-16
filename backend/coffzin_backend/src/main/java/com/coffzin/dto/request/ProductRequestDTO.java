dde87d5b0a4da693b4b12fb03c564745d0c4d484
Divansir-Junior <jscrobut989@gmail.com>
2026-02-15 21:24:05 -0300

    feat : ProductRequest class created

---
+++ backend/coffzin_backend/src/main/java/com/coffzin/dto/request/ProductRequestDTO.java
@@ -0,0 +1,28 @@
+package com.coffzin.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequestDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be greater than zero")
+    private Double price;
+
+    @NotNull(message = "Stock quantity is required")
+    @PositiveOrZero(message = "Stock cannot be negative")
+    private Integer stockQuantity;
+}
--- backend/coffzin_backend/src/main/java/com/coffzin/service/ProductService.java
+++ backend/coffzin_backend/src/main/java/com/coffzin/service/ProductService.java
@@ -2,7 +2,16 @@ package com.coffzin.service;
 
 import org.springframework.stereotype.Service;
 
+import com.coffzin.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    
 
 }
