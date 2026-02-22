294e87e31aa7f4a3979b40fc862077ff7bb13f1f
Divansir-Junior <jscrobut989@gmail.com>
2026-02-21 23:19:53 -0300

    feat : removed the urlParameter from the function

--- frontend/src/script/main.js
+++ frontend/src/script/main.js
@@ -2,11 +2,13 @@ import { initHomePage } from "./pages/home.js";
 import { initRegisterPage } from "./pages/register.js";
 import { initCarousel } from "./components/carousel.js";
 import { redirectToPages } from "./util/redPages.js";
+import { createUser } from "./services/userService.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initHomePage();
   initRegisterPage();
   initCarousel();
   redirectToPages();
+  createUser();
   if (window.lucide) lucide.createIcons();
 });
--- frontend/src/script/services/userService.js
+++ frontend/src/script/services/userService.js
@@ -1,6 +1,6 @@
 const API_URL = "http://localhost:8080/api/users";
 
-export async function createUser(data) {
+export async function createUser() {
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
