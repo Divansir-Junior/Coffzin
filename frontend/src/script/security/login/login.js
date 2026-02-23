f363db924090a98af45782bfa38e3410763cc77b
Divansir-Junior <jscrobut989@gmail.com>
2026-02-23 14:32:14 -0300

    feat(auth): create initial login function structure
    Implemented the initial scope and structural foundation
    for the user login functionality.
    
    This includes the base function setup and preparation
    for future authentication logic integration.

--- frontend/src/script/security/login/login.js
+++ frontend/src/script/security/login/login.js
@@ -1,1 +1,12 @@
 
+async function login () { 
+    const email = document.getElementById("login").value;
+    const password = document.getElementById("pass").value;
+
+    const response = await fetch ("" ,  {
+        method : "Post",
+        headers : {"Content-Type" : "application/json"},
+        body : JSON.stringify({email,password})
+   });
+
+}
